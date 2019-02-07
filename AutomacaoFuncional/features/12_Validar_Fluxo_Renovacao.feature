cc4a202e40f5f7621f21a2b82b4e3abd9691de77
leonardoBarcellosM <leonardo.barcellos@viaflow.com.br>
2019-02-07 09:50:33 -0200

    Teste 01

---
+++ AutomacaoFuncional/features/12_Validar_Fluxo_Renovacao.feature
@@ -0,0 +1,9 @@
+﻿Feature: 12_Validar_Fluxo_Renovacao
+
+#Fluxo de renovação 
+
+@01_ConsultarInstanciaCamunda
+Scenario: 01 Consultar instancia no Camunda e iniciar renovação
+Given Consulto se há intancia do cpf "37793090845" no camunda
+#When Inicio o processo de renovacao do cpf "37793090845"
+#Then Instancia deve ser inicializada com sucesso
---
+++ AutomacaoFuncional/tests/pages/CamundaApiActions.cs
@@ -0,0 +1,45 @@
+﻿using AutomacaoFuncional.tests.utils;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using System.Windows.Forms;
+
+namespace AutomacaoFuncional.tests.pages
+{
+    class CamundaApiActions
+    {
+        public bool ConsultarInstancia(string Cpf)
+        {
+            bool _result = false;
+            string Instancia = "";
+            string Validar = "";
+
+            try
+            {
+               Instancia =  CamundaApi.GetInstance(Cpf);
+                if (string.IsNullOrEmpty(Cpf))
+                {
+                    _result = true;
+                }
+                else
+                {
+                  Validar = CamundaApi.DeleteInstance(Instancia);
+                    //if (string.IsNullOrEmpty(Validar))
+                    //{
+                    //    _result = true;
+                    //}
+                    _result = true;
+                } 
+            }
+            catch
+            {
+
+            }
+
+            return _result;
+        }
+
+    }
+}
